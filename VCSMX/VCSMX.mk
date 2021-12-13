1fde241f8dd5073129760e3d9f8ea21e583e1e0a
bhyou <bhyou@foxmail.com>
2021-12-13 10:45:35 +0800

    add help

--- VCSMX/VCSMX.mk
+++ VCSMX/VCSMX.mk
@@ -5,35 +5,89 @@
 # mail        : bhyou@foxmail.com
 # Created Time: Mon 13 Dec 2021 08:43:41 AM CST
 #########################################################################
-.PHONY:clean file com mixed sim 
-clean:
-	@rm -fr simv* csrc* ucli* 64 AN* DVE* *.vpd  *.log
+
+########################################################################
+# The steps of compiling vhdl-verilog mixed design: 
+#     1. analyse hdl codes : 
+#     	- vhdlan -full64 -nc  -f vhdllist.f
+#     	- vlogan -full64 +v2k -f vloglist.f
+#     2. elaborate design  : vcs -full64 -debug_pp -top ${topDesign}
+#     3. simulation        : ./simv -gui 
+# Note : The simulation tools must be VCS-MX not VCS. 
+#        VCS not supports vhdl compile.
+########################################################################
+
+.PHONY:default help clean file vlogcom vhdlan mixecom 
+
+default: help
 
 
-#The following compile option is used for coverage.
+waveFormat ?= vpd
+topDesign  ?= top_tb
+comLog     ?= compile.log
+simLog     ?= sim_tb.log
+ 
+UseFileList?= yes
+
+# UseFileList ?= no
+# FILES += not_gate.v
+# FILES += not_gate_tb.v
+
+# coverage options
 coverOpt += -cm line+cond+tgl+fsm+branch+assert
-#The following compile option is used to interactive simulation on verdi and vcs
-comOpt += -lca -kdb
-comOpt += -full64 -sverilog +v2k 
-comOpt += -override_timescale=1ps/1ps
-logOpt += -l vcs_run.log
 
-wavefile ?= vpd
-ifeq ($(wavefile),vpd)
+# interactive simulation on verdi and vcs
+comOpts += -lca -kdb
+
+comOpts += -full64 -sverilog +v2k -l ${comLog}
+comOpts += -override_timescale=1ns/1ps
+comOpts += -debug_access+r
+
+#macroOpt += +define+Tdelay=200
+#macroOpt += +incdir+../rtl/include
+
+ifeq ($(waveFormat),vpd)
 	macroOpt += +define+VPD
-	comOpt += -debug_pp
 else
+	macroOpt += +define+fsdb
 	comOpt += -fsdb
 endif
 
-macroOpt += +define+Tdelay=200
-#macroOpt += +incdir+../rtl/include
+ifeq (${UseFileList}, yes)
+	FILES = -file $(filelist)
+	filelist = vloglist.f
+endif
 
-top ?= top_tb
-simOpt += -l tb_sim.log 
+help: 
+	@echo ======================================================================= 
+	@echo   "        " 
+	@echo   " USAGE: %make target_name_*              " 
+	@echo   "        " 
+	@echo   " ------------------------ DEBUG TARGETS ----------------------------" 
+	@echo   " file         => Generate file lsit of vhdl and verilog in ../directory" 
+	@echo   " clean        => Clean the intermediate files.                      " 
+	@echo   " vhdlcom      => Compile the TB and DUT writed by vhdl.             " 
+	@echo   " vlogcom      => Compile the TB and DUT writed by verilog / systemverilog." 
+	@echo   " mixcom       => Compile the TB and DUT writed by vhdl-verilog.           " 
+	@echo   " guiDve       => Run simulation interactively with DVE.          " 
+	@echo   " guiVerdi     => Run simulation interactively with Verdi.        " 
+	@echo   " dveCcov      => Brings up DVE for coverage reporting.           " 
+#	@echo   " run_*        => Run the simulation.                             " 
+#	@echo   " tb_gui_*     => Runs simulation interactively with TB Debugger. " 
+#	@echo   " new_gui_*    => Run new integrated debuggers.                " 
+#	@echo   " urg          => Make a coverage report for debug runs.       " 
+	@echo   "                                                                    " 
+	@echo   " -------------------- ADMINISTRATIVE TARGETS -----------------------" 
+	@echo   " help           => Displays this message.                         " 
+#	@echo   " initial        => Clean all files, including coverage files.    " 
+#	@echo   " tar            => Tar and zip kit and place at ../                 " 
+	@echo   "        " 
+	@echo   " e.g.   gmake test_1                                              " 
+	@echo ======================================================================= 
 
-FILES = -file $(filelist)
-filelist = vloglist.f
+clean:  
+	@rm -fr simv* csrc* ucli* 64 AN* DVE* *.vpd  *.log  vhdlcomLog
+
 file:
 	@touch vloglist.f
 	@find .. -name "*.*v" > vloglist.f
@@ -41,34 +95,34 @@ ifneq ($(wildcard ../*/*.vhd),)
 	@find .. -name  "*.vhd" > vhdllist.f 
 endif
 
-com:
-	vcs $(comOpt) $(logOpt) $(macroOpt) $(FILES) $(TOP)
+vlogcom:
+	vcs $(comOpt) $(macroOpt) $(FILES)
+ 
+vhdlcom: vhdllist.f
+	vhdlan -full64 -nc -f vhdllist.f
+	vcs -full64 -debug_pp -l ${comLog} -top ${topDesign}
 
-
-#The following three steps are used for vhdl-verilog mixed simulation.
-#The first two steps is used to analyse hdl coding.
-#The third step is used to elaborate the coding.
-mixed:filelist
+mixcom: vhdllist.f vloglist.f
 	vhdlan -fulll64 -nc vhdlist.f
 	vlogan -full64 +v2k -sverilog +define+syn_off vloglist.f 
-	vcs -full64 -debug_pp -top $(top) -l elab.log $(comOpt)
-cov:
-	@./simv $(simOpt) $(coverOpt) 
+	vcs -full64 -debug_pp -top $(topDesign) -l elab.log $(comOpt)
 
-dve:
-	@./simv $(simOpt) -gui & 
+guiDve: simv 
+	@./simv -l $(simLog) -gui & 
 
-verdi:
-	@./simv $(simOpt) -gui=verdi & 
+guiVerdi: simv
+	@./simv $(simOpt) -gui=verdi &
+ 
+dveCov: simv
+	@./simv $(simOpt) $(coverOpt) &
 
-view:
+viewCov: simv.vdb
 	dve -full64 -covdir simv.vdb 
-# view fsdb cmd: verdi -nologo -ssf *.fsdb 
-# view coverage report cmd: dve -full64 -cov -dir simv.vpd
 
-vhdl_com: vhdllist.f
-	vhdlan -full64 -nc -f vhdllist.f
-	vcs -full64 -debug_pp -l elab.log -top ${TOP}
-	
+viewFsdb:
+	verdi -nologo -ssf ${fsdbFile} &
+
+viewVpd:
+	dve -full64 -cov -dir simv.vpd &
 
  
